5f233df1c2c88718d5a06f1c19169cd5474bee21
ali.behrooz <ali.behrooz.2002@gmail.com>
2024-12-03 12:55:24 +0330

    add footer for admin

---
+++ admin/includes/footer.php
@@ -0,0 +1,8 @@
+    <!-- Vendor JS -->
+    <script src="src\js\vendors.min.js"></script>
+    <script src="src\js\pages\chat-popup.js"></script>
+    <script src="assets\icons\feather-icons\feather.min.js"></script>
+    <script src="assets\vendor_components\apexcharts-bundle\apexcharts.min.js"></script>
+    <script src="style/js/dashboard-light.js"></script>
+    <script src="src\js\template.js"></script>
+    <script src="src\js\pages\dashboard.js"></script>
