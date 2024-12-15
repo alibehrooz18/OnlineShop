9ab86d2b0a749e9dab936906c0267be90211364e
ali.behrooz <ali.behrooz.2002@gmail.com>
2024-12-15 14:31:30 +0330

    add some code for auto load models and now it's commented

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -4,3 +4,16 @@ session_start();
 ?>
 <?php include_once __DIR__ . '/../../includes/db.php'; ?>
 
+<?php
+// spl_autoload_register('autoLoader');
+// function autoLoader($class_name) {
+//     $pass = "../model/";
+//     $extention = "_model.php";
+//     $full_path = $pass . $class_name . $extention;
+
+//     if(!file_exists($full_path)) {
+//         return false;
+//     }
+
+//     include_once $full_path;
+// }
