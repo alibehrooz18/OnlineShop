f45016570a34bacd4f11998ebf10e458c28ca1ca
ali.behrooz <ali.behrooz.2002@gmail.com>
2024-12-16 13:41:25 +0330

    add some more data

--- admin/controllers/details_controller.php
+++ admin/controllers/details_controller.php
@@ -11,15 +11,18 @@ if (isset($_GET['c_id'])) {
     $detail_id = $course_details['course_id'];
     $detail_category_id = $course_details['course_category_id'];
     $detail_title = $course_details['course_title'];
+    $detail_image = $course_details['course_image'];
     $detail_content = $course_details['course_content'];
     $detail_date = $course_details['course_date'];
     $detail_tags = $course_details['course_tags'];
     $detail_price = $course_details['course_price'];
     $detail_status = $course_details['course_status'];
+    $detail_url = $course_details['course_url'];
 
     // Fetch category details if available
     if (isset($detail_category_id)) {
         $category_details = getCategoryDetails($connection, $detail_category_id);
+        $category_id = $category_details['cat_id'];
         $category_title = $category_details['cat_title'];
     }
 
