05d5aeb0f774354f67360001b99057564876c9e0
ali.behrooz <ali.behrooz.2002@gmail.com>
2024-12-17 14:55:49 +0330

    change database connection to singleton

--- includes/db.php
+++ includes/db.php
@@ -1,20 +1,41 @@
 <?php
-// Connect to database
-$db['db_host'] = 'localhost';
-$db['db_user'] = 'root';
-$db['db_pass'] = '';
-$db['db_name'] = 'eduom';
+class Database {
+    private static $instance = null;
+    private $connection;
 
-foreach ($db as $key => $value) {
-    if(!defined(strtoupper($key))) {
-        define(strtoupper($key), $value);
+    // Information for database connection
+    private $db_host = 'localhost';
+    private $db_user = 'root';
+    private $db_pass = '';
+    private $db_name = 'eduom';
+
+    private function __construct() {
+        $this->connection = mysqli_connect($this->db_host, $this->db_user, $this->db_pass, $this->db_name);
+
+        if (!$this->connection) {
+            die("Database connection failed: " . mysqli_connect_error());
+        }
+    }
+
+    // Use singleton for get instance
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    // Use method for return connection to database
+    public function getConnection() {
+        return $this->connection;
     }
 }
 
-// Make connection
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+// Use singleton for connecting to database
+$dbInstance = Database::getInstance();
+$connection = $dbInstance->getConnection();
 
-//Check connection
-// if ($connection){
-//     echo "connected";
+// Test database
+// if ($connection) {
+//     echo "Database connected successfully.";
 // }
