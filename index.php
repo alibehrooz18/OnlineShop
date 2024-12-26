99b9e4598d0c3521aef2a34260ed59ce197a36c5
ali.behrooz <ali.behrooz.2002@gmail.com>
2024-12-26 22:51:57 +0330

    update link

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
 
     <?php include "./includes/header.php"; ?>
-    <?php include "./app/controllers/main_ctrl.php"; ?>
+    <?php include "./app/controllers/MainCtrl.php"; ?>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
--- log-in.php
+++ log-in.php
@@ -4,7 +4,7 @@
 <head>
 
     <?php include "includes/header.php"; ?>
-    <?php include "./app/controllers/account_ctrl.php"; ?>
+    <?php include "./app/controllers/AccountCtrl.php"; ?>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
